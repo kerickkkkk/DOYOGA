3d9ae03bf503986a03f5232b2ffcf8e46270fc2d
eric <howcrazys@gmail.com>
2021-09-05 16:27:21 +0800

    feat: 加入 swiper 瑜珈空間完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  var courseRecommendationSwiper = new Swiper(".courseRecommendationSwiper", {
+    slidesPerView: 1.2,
+    slidesPerColumn: 1,
+    spaceBetween: 30,
+    breakpoints:{
+      768: {
+        slidesPerView: 3,
+        slidesPerColumn: 1,
+      },
+      992: {
+        slidesPerView: 4.2,
+        slidesPerColumn: 1,
+      }
+    }
+  });
 });
