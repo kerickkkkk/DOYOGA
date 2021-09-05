88f587a812ae71c8483b6bf655cf5c56d5aed39f
eric <howcrazys@gmail.com>
2021-09-06 00:02:21 +0800

    Update 2021-09-05T16:02:09.427Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,42 @@
 "use strict";
 
 $(function () {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30
+  var listenSwiper = new Swiper(".listenSwiper", {
+    slidesPerView: 1,
+    slidesPerColumn: 3,
+    spaceBetween: 8,
+    slidesPerColumnFill: 'row',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2.1,
+        slidesPerColumn: 2,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 30
+      }
+    }
+  });
+  var courseRecommendationSwiper = new Swiper(".courseRecommendationSwiper", {
+    slidesPerView: 1.2,
+    slidesPerColumn: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        slidesPerColumn: 1
+      },
+      992: {
+        slidesPerView: 4.2,
+        slidesPerColumn: 1
+      }
+    }
   });
 });
 //# sourceMappingURL=all.js.map
